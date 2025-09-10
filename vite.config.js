e3e0ba2bdde1f788d613fe13f8907d7dcf7ad5b6
bhanu <bhanusingh0605@gmail.com>
2025-09-11 01:56:43 +0530

    netlify config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,17 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+        assetFileNames: "assets/[name].[hash].[ext]",
+      },
+    },
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  base: "/",
 });
